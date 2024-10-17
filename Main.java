8641fc2ae8252e36b14cb2dba75947829cf04fd6
gostforyou <igor.nurgal@gmail.com>
2024-10-17 22:40:35 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
 
+public class Main {
+    // Объявляем объект класса Scanner для ввода данных
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Вводим данные
+        System.out.println("Введите длины отрезков A, B и C:");
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        // Проверка можно ли сделать треугольник
+        if ((A + B > C) && (A + C > B) && (B + C > A)) {
+            System.out.println("Из сторон можно сделать треугольник");
+            // Определяем вид
+            if((A == B) && (B == C) && (A == C)) {
+                System.out.println("Треугольнниик равносторонний");
+            } else if (A == B || B == C || A == C) {
+                System.out.println("Треугольник равнобедренный");
+            }
+            int max = Math.max(A, Math.max(B, C));
+            int sumOfSquares = A * A + B * B + C * C - max * max;
+            if (max * max == sumOfSquares) {
+                System.out.println("Треугольник прямоугольный");
+            } else if (!(A == B || B == C || A == C)) {
+                System.out.println("Треугольник разносторонний");
+            }
+        } else {
+            System.out.println("Из сторон нельзя сделать треугольник");
+        }
+        scanner.close();    
+    }
+}
